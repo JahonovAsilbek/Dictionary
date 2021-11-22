b3946db655c9d235d72d4923db2d587f5314d057
JahonovAsilbek <asilbekjahonov2001@gmail.com>
2021-11-23 01:21:03 +0500

    added auto complete text view

--- app/src/main/java/uz/pdp/dictionary/ui/home/HomeFragment.kt
+++ app/src/main/java/uz/pdp/dictionary/ui/home/HomeFragment.kt
@@ -1,9 +1,14 @@
 package uz.pdp.dictionary.ui.home
 
+import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import uz.pdp.dictionary.MainActivity
@@ -28,6 +33,32 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
 
         drawerClick()
+
+        val adapter by lazy {
+            ArrayAdapter<String>(
+                binding.root.context,
+                R.layout.simple_dropdown_item_1line
+            )
+        }
+        adapter.addAll("ashdbs", "djhsdc", "shcbsdkhc", "dkshbskd", "sdcskcsd", "sdjcbsdh")
+
+        binding.searchView.setAdapter(adapter)
+        binding.searchView.doOnTextChanged { text, _, _, _ ->
+            // Call api to find search results for your text:
+
+//            callApiFor(text.toString())
+        }
+
+        binding.searchView.setOnEditorActionListener { textView, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                // call an API to find your results
+                Toast.makeText(binding.root.context, "bingoo", Toast.LENGTH_SHORT).show()
+//                searchResults(textView.text.toString())
+                return@setOnEditorActionListener true
+            }
+
+            false
+        }
 
 
         return binding.root
